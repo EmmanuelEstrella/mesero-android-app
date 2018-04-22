b336c0679ce0ef17cef2df3866507166f71c14c6
Moises Ynfante <moisesynfan@gmail.com>
2018-04-22 16:06:14 -0400

    Started notification logic.

--- app/src/main/java/com/example/emmanuel/myapplication/Logic/FireBaseMessagingService.java
+++ app/src/main/java/com/example/emmanuel/myapplication/Logic/FireBaseMessagingService.java
@@ -1,8 +1,14 @@
 package com.example.emmanuel.myapplication.Logic;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.nfc.Tag;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
+import com.example.emmanuel.myapplication.MainActivity;
+import com.example.emmanuel.myapplication.R;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -31,7 +37,30 @@ public class FireBaseMessagingService extends FirebaseMessagingService {
             Log.d(Tag, "Message Notification Body: " + remoteMessage.getNotification().getBody());
         }
 
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "0")
+                .setContentTitle("Notification")
+                .setContentText("Message")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define
+        notificationManager.notify(0, mBuilder.build());
+
+
         // Also if you intend on generating your own notifications as a result of a received FCM
         // message, here is where that should be initiated. See sendNotification method below.
+
     }
+
+
+
+
 }
